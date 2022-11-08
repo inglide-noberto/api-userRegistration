e5ae1589ab9467a59df1ab7832fb16f0431b93eb
Inglide Noberto <inglidenoberto961@gmail.com>
2022-11-07 21:11:02 -0300

    creating routes for CRUD

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
+const users = [{
+    name: 'Inglide',
+    email: 'teste@teste.com',
+    password: '123456'
+}];
 
-server.get('/test', (req, res) => {
-    return res.json(
-        { message: 'Hello world' }
-    );
+//get all users
+server.get('/users', (req, res) => {
+    return res.json(users);
+});
+//get one users
+server.get('/users/:index', (req, res) => {
+    return res.json(req.users);
+});
+
+server.post('/users' ,(req, res) => {
+    const {name} = req.body;
+    users.push(name);
+
+    return res.json(users);
 });
 
 server.listen(3000);
